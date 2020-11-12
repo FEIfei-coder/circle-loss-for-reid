293e29df2cf0bf9f9afb4b5a8fae850d7ca8776c
FEIfei <857357502@qq.com>
2020-11-12 10:23:09 +0800

    Update losses.py
    del

--- losses.py
+++ losses.py
@@ -166,20 +166,6 @@ class RingLoss(nn.Module):
 
 
 
-def convert_label_to_similarity(normed_feature: Tensor, label: Tensor) -> Tuple:
-    similarity_matrix = normed_feature @ normed_feature.transpose(1, 0)
-    label_matrix = label.unsqueeze(1) == label.unsqueeze(0)
-    positive_matrix = label_matrix.triu(diagonal=1)
-    negative_matrix = label_matrix.logical_not().triu(diagonal=1)
-
-    similarity_matrix = similarity_matrix.view(-1)
-    positive_matrix = positive_matrix.view(-1)
-    negative_matrix = negative_matrix.view(-1)
-    return similarity_matrix[positive_matrix], similarity_matrix[negative_matrix]
-
-
-
-
 class CircleLoss(nn.Module):
 	'''
 	a circle loss implement, simple and crude
